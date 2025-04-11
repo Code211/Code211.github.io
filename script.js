bc48698cabe8c8ae11c55b82b2cdd90389d04024
V-Paritosh <paritoshv08@gmail.com>
2025-04-10 23:23:29 -0500

    final updates

--- script.js
+++ script.js
@@ -18,12 +18,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   anime({
-    targets: ".btn-primary",
+    targets: ".countdown-box",
     translateY: [50, 0],
     opacity: [0, 1],
     easing: "easeOutExpo",
     duration: 1500,
     delay: 1000,
+  });
+
+  anime({
+    targets: ".btn-primary",
+    translateY: [50, 0],
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1500,
+    delay: 1500,
   });
 
   // Helper function for animation
@@ -58,7 +67,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document
     .querySelectorAll(".schedule-row, .accordion-item, #sponsors img")
-    .forEach((section, index) => {
+    .forEach((section) => {
       observer.observe(section);
     });
+  
+  const countdownEl = document.getElementById("countdown");
+  const countDownDate = new Date("Apr 12, 2025 08:00:00").getTime();
+
+  const x = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    if (distance < 0) {
+      clearInterval(x);
+      countdownEl.innerHTML =
+        "<h3 class='font-weight-bold'>BEGIN CODING!</h3>";
+      return;
+    }
+
+    const days = Math.floor(
+      (distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+    );
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countdownEl.innerHTML = `
+      <div class="time-box">
+        <p class="h3 font-weight-bold">${days}</p>
+        <p class="small">Days</p>
+      </div>
+      <div class="divider">:</div>
+      <div class="time-box">
+        <p class="h3 font-weight-bold">${hours}</p>
+        <p class="small">Hours</p>
+      </div>
+      <div class="divider">:</div>
+      <div class="time-box">
+        <p class="h3 font-weight-bold">${minutes}</p>
+        <p class="small">Minutes</p>
+      </div>
+      <div class="divider">:</div>
+      <div class="time-box">
+        <p class="h3 font-weight-bold">${seconds}</p>
+        <p class="small">Seconds</p>
+      </div>
+    `;
+  }, 1000);
 });
